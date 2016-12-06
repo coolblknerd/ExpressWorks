eabc1a7f3c6656e1b8c9e27222b509bdabc3c96b
Reggie Davis <reginald.davis88@gmail.com>
2016-12-05 23:06:38 -0600

    Completed Ex. 1 & 2

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,17 @@
+// HELLO WORLD!
+//  Exercise 1 of 8
+//
+// Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
+//
+// The port number will be provided to you by expressworks as the first argument of
+// the application, ie. process.argv[2].
+
+----------------------
+
 const express = require('express');
 var app = express(),
     port = process.argv[2];
 
-app.get('/', function(req, res) {
+app.get('/home', function(req, res) {
   res.end('Hello World!');
 }).listen(port);
